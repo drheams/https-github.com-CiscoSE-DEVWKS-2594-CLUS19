c86b7805c3614ed5516f69b884e423a492b71b3f
Tim Miller <timmil@cisco.com>
2019-06-11 09:17:58 -0700

    Nexus 9000v requires tcp/80 enabled for NXAPI

--- n9kv/enable_nxapi.sh
+++ n9kv/enable_nxapi.sh
@@ -7,6 +7,7 @@ if [ "$?" -ne 0 ]; then
 fi
 
 printf "conf t\nfeature nxapi\nend" | vagrant ssh
+printf "conf t\nnxapi http port 80\nend" | vagrant ssh
 printf "conf t\nboot nxos bootflash:nxos.9.2.3.bin\nend" | vagrant ssh
 printf "conf t\ncopy run start\nexit" | vagrant ssh
 
